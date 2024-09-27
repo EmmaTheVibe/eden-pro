51135d4c010f967c00936fcfe95fe2734c16182f
EmmaTheVibe <peteremmanuel316@gmail.com>
2024-09-27 18:53:37 +0100

    updates

--- src/firebase/firebaseService.js
+++ src/firebase/firebaseService.js
@@ -1,24 +1,19 @@
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 
-// Function to submit email
 export const submitEmail = async (email) => {
-  const emailsDocRef = doc(db, "emails", "emailList"); // The document will be "emailList" in the "emails" collection
+  const emailsDocRef = doc(db, "emails", "emailList");
 
   try {
-    // Get the existing document
     const docSnapshot = await getDoc(emailsDocRef);
 
     if (docSnapshot.exists()) {
       const emailArray = docSnapshot.data().emails || [];
 
-      // Add the new email to the array
       const updatedEmails = [...emailArray, email];
 
-      // Update the document with the new email array
       await setDoc(emailsDocRef, { emails: updatedEmails }, { merge: true });
     } else {
-      // If the document does not exist, create a new one with the email
       await setDoc(emailsDocRef, { emails: [email] });
     }
 
