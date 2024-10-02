2ad819495820e2bec782478a93d2d718f7e35d21
EmmaTheVibe <peteremmanuel316@gmail.com>
2024-10-02 16:23:25 +0100

    scroll fix

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,10 +1,26 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+
+// const ScrollToTop = () => {
+//   const { pathname } = useLocation();
+
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [pathname]);
+
+//   return null;
+// };
+
+// export default ScrollToTop;
+
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
+    // This will scroll to the top only on navigation between routes
     window.scrollTo(0, 0);
   }, [pathname]);
 
